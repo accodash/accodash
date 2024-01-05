fe08a31cc4d0b055203592f75a7cffab54276d40
palinkiewicz <pogromca.ap@gmail.com>
2024-01-05 19:51:03 +0100

    Remove the test route

--- routes/web.php
+++ routes/web.php
@@ -29,37 +29,3 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-
-Route::get('/test', function () {
-    \Illuminate\Support\Facades\Artisan::call('migrate:fresh');
-    /** @var \App\Models\Country $country */
-    $country = App\Models\Country::create(['name' => 'Poland']);
-    /** @var \App\Models\City $city */
-    $city = $country->cities()->create(['name' => 'Łódź']);
-    /** @var \App\Models\BuildingType $type */
-    $type = \App\Models\BuildingType::create(['name' => 'Hotel']);
-    /** @var \App\Models\Building $building */
-    $building = $city->buildings()->create([
-        'name' => 'Test building',
-        'description' => 'Test description',
-        'address' => 'Kolejowa 13',
-        'main_image_url' => 'url.jpg',
-        'type_id' => $type->id,
-        'city_id' => 2
-    ]);
-    $building->save();
-    /** @var \App\Models\Accommodation $acco */
-    $acco = $building->accommodations()->create([
-        'info' => 'lore',
-        'price' => 3000,
-        'adults_capacity' => 3,
-        'children_capacity' => 2
-    ]);
-//    /** @var \App\Models\Booking $booking */
-//    $booking = $acco->bookings()->create([
-//        'check_in' => now(),
-//        'check_out' => now()->addDay()
-//    ]);
-//    $booking->refresh();
-//    dump($booking->created_at);
-});
